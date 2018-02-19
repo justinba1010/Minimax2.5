3f21c311c0c1554e2d428b9864aeb0dfa472e366
Justin Baum <justinba1010@gmail.com>
2018-02-18 20:37:57 -0500

    Finishing through Node.

--- BaseBoard.java
+++ BaseBoard.java
@@ -1,3 +1,17 @@
+import java.util.*;
+//Justin Baum
+
 public class BaseBoard {
-  
+
+  public int evaluate() {
+    return 0;
+  }
+
+  public ArrayList<Object> generateLegalMoves(boolean turn) {
+    return null;
+  }
+
+  public Object deepCopy() {
+    return null;
+  }
 }
--- Node.java
+++ Node.java
@@ -4,12 +4,46 @@ import java.util.*;
 public class Node<Move extends BaseMove, Board extends BaseBoard> {
   public Board board;
   public int value;
-  public Node bestMove;
-  public Node lastMove;
+  public Move bestMove;
+  public Node bestNode;
+  public Move lastMove;
+  public Node lastNode;
+  public boolean turn;
   public ArrayList<Node<Move, Board>> children;
 
   public Node() {
+    board = (Board)(new BaseBoard());
+    value = board.evaluate();
+    bestMove = null;
+    bestNode = null;
+    lastMove = null;
+    lastNode = null;
+    children = new ArrayList<Node<Move, Board>>();
+  }
+  public Node(Board aBoard, Node aNode, Move aMove) {
+    board = aBoard;
+    value = board.evaluate();
+    bestMove = null;
+    bestNode = null;
+    lastMove = aMove;
+    lastNode = aNode;
+    children = new ArrayList<Node<Move, Board>>();
+  }
 
+  public void generate(int depth) {
+    //Get arraylist of moves made, for easy search so we don't double up.
+    ArrayList<Move> movesMade = new ArrayList<Move>();
+    for(Node child : children) {
+      movesMade.add((Move)child.lastMove);
+    }
+    //Make new moves, and check to see if it was already made
+    for(Object o : board.generateLegalMoves(turn)) {
+      Move move = (Move)o;
+      if(!movesMade.contains(move)){ //Make sure it's not in there
+        Board newBoard = (Board)board.deepCopy();
+        Node newNode = new Node(newBoard, this, move);
+      }
+    }
   }
 
 
