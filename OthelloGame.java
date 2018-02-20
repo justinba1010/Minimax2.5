76a42d0dbb5e393f406c69ea714090aef6ee5e95
Justin Baum <justinba1010@gmail.com>
2018-02-20 01:21:05 -0500

    Playing with casting before bed.

--- OthelloGame.java
+++ OthelloGame.java
@@ -1,6 +1,9 @@
 public class OthelloGame {
   public static void main(String[] args) {
     OthelloBoard board = new OthelloBoard();
+    //Testing the morphism nonsense, it functions as predicted with the pointer remaining intact, so when it gets recast the values stay the same.
+    Object a = (Object)(board);
+    board = (OthelloBoard) a;
     System.out.println(board);
     OthelloMove move = new OthelloMove(true,3,5);
     System.out.println(board.isLegalMove(move));
