ecfad16e19816d7074c90ab91ede637ea9fe3201
Justin Baum <justinba1010@gmail.com>
2018-02-20 00:04:44 -0500

    Working on Othello.

--- OthelloBoard.java
+++ OthelloBoard.java
@@ -34,7 +34,12 @@ public class OthelloBoard extends BaseBoard {
 
   public ArrayList<OthelloMove> generateLegalMoves(boolean turn) {
     ArrayList<OthelloMove> legalMoves = new ArrayList<OthelloMove>();
+    //TODO
     return legalMoves;
+  }
+
+  public boolean isLegalMove(OthelloMove move) {
+    return !takeBlocks(move).isEmpty();
   }
 
   public OthelloBoard deepCopy() {
@@ -58,5 +63,73 @@ public class OthelloBoard extends BaseBoard {
     }// for x
     return s;
   }//toString
+
+  //Insider Stuff
+
+
+  private ArrayList<int[]> vectorBlocks(int x1, int y1, int[] vector) {
+    ArrayList<int[]> blocks = new ArrayList<int[]>();
+    while(onBoard(x1,y1)) {
+      int[] newspaces = addVector(x1, y1, vector);
+      x1 = newspaces[0];
+      y1 = newspaces[1];
+      if(onBoard(x1,y1)) {
+        int[] block = {x1,y1};
+        blocks.add(block);
+      }
+    }
+    return blocks;
+  }//vectorBlocks
+
+  private boolean onBoard(int x, int y) {
+    return (x >= 0) && (y >= 0) && (x < size) && (y < size);
+  }//onBoard
+
+  private boolean onBoard(int[] xy) {
+    if(xy.length != 2) return false;
+    return onBoard(xy[0],xy[1]);
+  }//onBoard
+
+  private int[] addVector(int x1, int y1, int[] vector) {
+    if(vector.length != 2) return new int[0];
+    int[] newblock = {x1+vector[0],y1+vector[1]};
+    return newblock;
+  }//addVector
+
+  private int whoIs(int[] xy) {
+    if(xy.length != 2) return 0;
+    if(!onBoard(xy)) return 0;
+    return gameboard[xy[0]][xy[1]];
+  }//whoIs
+
+  public ArrayList<int[]> takeBlocks(OthelloMove move) {
+    //So we need at least one vector to have the other player indefinitely until we hit the player who made the move.
+    int startx = move.x;
+    int starty = move.y;
+    ArrayList<int[]> takeBlocks = new ArrayList<int[]>();
+    for(int[] vector : vectors) {
+      ArrayList<int[]> blocks = new ArrayList<int[]>();
+      boolean sandwich = false; //Check to make sure the pieces we are taking are sandwiched
+      for(int[] block : vectorBlocks(startx, starty, vector)) {
+        //Check if block is enemy, else we just break
+        if(whoIs(block) == ((move.turn) ? 1 : -1)) {//If it is the same player as the move maker, we just break.
+          break;
+        }
+        if(whoIs(block) == ((move.turn) ? -1 : 1)) {//If it is the opposite player we want to add this block.
+          sandwich = true;
+        } else if(whoIs(block) == 0) {//If it is not a taken spot, then we can cut our search because we never got a sandwich.
+          sandwich = false;
+          break;
+        }// else if
+        blocks.add(block);//Add block because we did not hit our conditions
+      }//for block
+      if(sandwich) {
+        for(int[] block : blocks) {
+          takeBlocks.add(block);
+        }//for block
+      }//if sandwich
+    }//for vector
+    return takeBlocks;
+  }
 
 }
